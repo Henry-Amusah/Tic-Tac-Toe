a4679264ae06214ef0f04d377ecf7304e0c48c3d
Henry-Amusah <henry.amusah@amalitech.org>
2023-10-17 14:43:41 +0000

    Attached newGame() interface

--- App.js
+++ App.js
@@ -1,45 +1,108 @@
+'use strict';
+
 const logo  = document.querySelector('.logo');
 const infoBoard = document.querySelector('.player-turn-info');
 const restartBtn = document.querySelector('.restart');
-const container = document.querySelector('.body');
-const playerSelect = document.querySelector('.player-select-option');
+const gameBoard = document.querySelector('.body');
+const gridContainer = gameBoard.querySelector('.grid');
+const playerSelectBoard = document.querySelector('.player-select-board');
+const playerSelectOption = document.querySelector('.player-select-option');
+const newGameBtn = document.querySelector('.new-game');
+const scoreBoard = document.querySelector('.score-board');
 
+
+const leftPlayerName = document.querySelector('.x-score-board .p1-name');
+const rightPlayerName = document.querySelector('.o-score-board .p2-name');
+const leftScoreValue = document.querySelector('.x-score-board .x-score-value');
+const rightScoreValue = document.querySelector('.o-score-board .o-score-value');
+const tieScoreValue = document.querySelector('.tied-score-value');
+
+let leftScoreCount = Number(leftScoreValue.textContent);
+let rightScoreCount = Number(rightScoreValue.textContent);
+let tieScoreCount = Number(tieScoreValue.textContent);
+leftScoreCount = 0;
+tieScoreCount = 0
+rightScoreCount = 0
+
+let p1_symbol = 'O', p2_symbol, cpu_symbol;
 
 class Game {
   constructor(){
-    playerSelect.addEventListener('click', this.togglePlayerLogo);
+    playerSelectOption.addEventListener('click', this.togglePlayerLogo.bind(this));
+    newGameBtn.addEventListener('click', this.newGame.bind(this));
   }
+
+  activatePlayer(bg, playerSymbol){
+    bg.classList.add('bg-toggle');
+    bg.classList.remove('bg-hover');
+    bg.children[0].setAttribute('src', `assets/icon-${playerSymbol}-outline-white.svg`);
+  }
+
+  deactivatePlayer(bg, playerSymbol){
+    bg.classList.remove('bg-toggle');
+    bg.classList.add('bg-hover');
+    bg.children[0].setAttribute('src', `assets/icon-${playerSymbol}-white.svg`);
+  }
+
 
   togglePlayerLogo(e){
     let x_bg = document.querySelector('.x-mark'), o_bg = document.querySelector('.o-mark');
     const { target } = e;
     if(target.classList.contains('x-mark') || target.closest('.x-mark')){
       if(target.classList.contains('bg-hover') || target.closest('.x-mark').classList.contains('bg-hover')){
-        // Activate selected state for X player
-        x_bg.classList.add('bg-toggle');
-        x_bg.classList.remove('bg-hover');
-        x_bg.children[0].setAttribute('src', 'assets/icon-x-outline-white.svg');
-        
-        // Deactivate selected state for O player
-        o_bg.classList.remove('bg-toggle');
-        o_bg.classList.add('bg-hover');
-        o_bg.children[0].setAttribute('src', 'assets/icon-o-white.svg');
+        this.activatePlayer(x_bg, 'x'); // Activate selected state for X player
+        this.deactivatePlayer(o_bg, 'o'); // Deactivate selected state for O player
+        p1_symbol = 'X';
       }
     } 
     if(target.classList.contains('o-mark') || target.closest('.o-mark')){
       if(target.classList.contains('bg-hover') || target.closest('.o-mark').classList.contains('bg-hover')){
-        // Activate selected state for O player
-        o_bg.classList.add('bg-toggle');
-        o_bg.classList.remove('bg-hover');
-        o_bg.children[0].setAttribute('src', 'assets/icon-o-outline-white.svg');
-        
-        // Deactivate selected state for O player
-        x_bg.classList.remove('bg-toggle');
-        x_bg.classList.add('bg-hover');
-        x_bg.children[0].setAttribute('src', 'assets/icon-o-white.svg');
+        this.activatePlayer(o_bg, 'o'); // Activate selected state for O player
+        this.deactivatePlayer(x_bg, 'x'); // Deactivate selected state for X player
+        p1_symbol = 'O';
       }
     }
+    //console.log(p1);
   }
+
+
+  newGame(e){
+    const { target } = e;
+    if(target.classList.contains('new-game-vs-cpu')){
+      cpu_symbol = p1_symbol === 'X' ? 'O' : 'X';
+      if(p1_symbol === 'X'){
+        leftPlayerName.textContent = `${p1_symbol} (YOU)`;
+        rightPlayerName.textContent = `${cpu_symbol} (CPU)`;
+      }else {
+        rightPlayerName.textContent = `${p1_symbol} (YOU)`;
+        leftPlayerName.textContent = `${cpu_symbol} (CPU)`;
+      }
+      //alert(`playing with cpu with ${p1_symbol} as you and ${cpu_symbol} as cpu`);
+    }
+    if(target.classList.contains('new-game-vs-player')){
+      p2_symbol = p1_symbol === 'X' ? 'O' : 'X';
+      if(p1_symbol === 'X'){
+        leftPlayerName.textContent = `${p1_symbol} (P1)`;
+        rightPlayerName.textContent = `${p2_symbol} (P2)`;
+      }else {
+        rightPlayerName.textContent = `${p1_symbol} (P1)`;
+        leftPlayerName.textContent = `${p2_symbol} (P2)`;
+      }
+      //alert(`playing with player with ${p1_symbol} as p1 and ${p2_symbol} as p2`);
+    }
+    leftScoreValue.textContent = leftScoreCount;
+    tieScoreValue.textContent = tieScoreCount; 
+    rightScoreValue.textContent = rightScoreCount;
+
+    [scoreBoard, gridContainer, infoBoard, restartBtn].forEach(item => {
+      item.classList.remove('hidden');
+    });
+    [newGameBtn, playerSelectBoard].forEach(item => {
+      item.classList.add('hidden');
+    });
+    logo.classList.remove('center');
+
+  };
 
 }
 
